aed64c4fac23fb5f77ede3a69614b58cd160e333
danielm <danielmontiel1600@gmail.com>
2024-04-05 15:24:50 -0500

    Create reference of db and model

---
+++ SimulacroCSharp/SimulacroCSharp.Mvc/Data/BaseContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SimulacroCSharp.Mvc.Models;
+
+namespace SimulacroCSharp.Mvc.Data
+{
+  public class BaseContext : DbContext
+  {
+    public BaseContext (DbContextOptions <BaseContext> options) : base (options)
+    { } 
+
+    // Reference model and database entity
+    public DbSet<Company> Companies {get; set;}
+  }
+}
