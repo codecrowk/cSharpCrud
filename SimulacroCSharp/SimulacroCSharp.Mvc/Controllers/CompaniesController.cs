6387225dac8e7433b29c5394a9dac441045086a8
danielm <danielmontiel1600@gmail.com>
2024-04-05 18:17:34 -0500

    Make create in companies

--- SimulacroCSharp/SimulacroCSharp.Mvc/Controllers/CompaniesController.cs
+++ SimulacroCSharp/SimulacroCSharp.Mvc/Controllers/CompaniesController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SimulacroCSharp.Mvc.Data;
+using SimulacroCSharp.Mvc.Models;
 
 namespace SimulacroCSharp.Mvc.Controllers 
 {
@@ -18,5 +19,24 @@ namespace SimulacroCSharp.Mvc.Controllers
       return View(await _context.Companies.ToListAsync());
     }
 
+    public async Task<IActionResult> Detail (int Id)
+    {
+      return View(await _context.Companies.FirstOrDefaultAsync(register => register.Id == Id));
+    }
+
+
+    public IActionResult Create ()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Post (Company c)
+    {
+      Console.WriteLine($"User info: {c}");
+      _context.Companies.Add(c);
+      _context.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
--- SimulacroCSharp/SimulacroCSharp.Mvc/Models/Company.cs
+++ SimulacroCSharp/SimulacroCSharp.Mvc/Models/Company.cs
@@ -4,13 +4,13 @@ namespace SimulacroCSharp.Mvc.Models
   {
     // Remember put getters and setters
     public int Id {get; set;}
-    public string Name {get; set;}
-    public string Nit {get; set;}
-    public string Address {get; set;}
-    public string Description {get; set;}
-    public string Logo {get; set;}
-    public string Phone {get; set;}
-    public string LegalRepresentative {get; set;}
-    public int SectorId {get; set;}
+    public string? Name {get; set;}
+    public string? Nit {get; set;}
+    public string? Address {get; set;}
+    public string? Description {get; set;}
+    public string? Logo {get; set;}
+    public string? Phone {get; set;}
+    public string? LegalRepresentative {get; set;}
+    public int? SectorId {get; set;}
   }
 }
