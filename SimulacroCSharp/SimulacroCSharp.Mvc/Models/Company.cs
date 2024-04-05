cffba0d0da8638479ba51257791e7f2638bf279e
danielm <danielmontiel1600@gmail.com>
2024-04-05 15:37:35 -0500

    Make company model

--- SimulacroCSharp/SimulacroCSharp.Mvc/Models/Company.cs
+++ SimulacroCSharp/SimulacroCSharp.Mvc/Models/Company.cs
@@ -6,7 +6,7 @@ namespace SimulacroCSharp.Mvc.Models
     public int Id {get; set;}
     public string Name {get; set;}
     public string Nit {get; set;}
-    public string Addres {get; set;}
+    public string Address {get; set;}
     public string Description {get; set;}
     public string Logo {get; set;}
     public string Phone {get; set;}
