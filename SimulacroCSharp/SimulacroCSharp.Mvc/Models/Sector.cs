6f596de8c7b0f1374505706a0201a2a1ca143e66
danielm <danielmontiel1600@gmail.com>
2024-04-05 15:50:56 -0500

    make sector model

---
+++ SimulacroCSharp/SimulacroCSharp.Mvc/Models/Sector.cs
@@ -0,0 +1,11 @@
+namespace SimulacroCSharp.Mvc.Models
+{
+  public class Sector
+  {
+    // Remember put getters and setters
+    public int Id {get; set;}
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public string Autor {get; set;}
+  }
+}
