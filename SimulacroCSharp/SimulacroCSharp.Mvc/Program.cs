1f78a9cf28ad235de56c779aec2fd0387611d428
danielm <danielmontiel1600@gmail.com>
2024-04-05 15:50:38 -0500

    setup database settings
    setup in .appsettings and Program

--- SimulacroCSharp/SimulacroCSharp.Mvc/Program.cs
+++ SimulacroCSharp/SimulacroCSharp.Mvc/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using SimulacroCSharp.Mvc.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Personal service
+builder.Services.AddDbContext<BaseContext> ( options =>
+    options.UseMySql
+    (
+        builder.Configuration.GetConnectionString("MySqlConnection"),
+        Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.20-mysql")
+    )
+);
 
 var app = builder.Build();
 
