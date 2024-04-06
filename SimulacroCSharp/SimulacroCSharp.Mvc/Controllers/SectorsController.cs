8991cd910258cb8a73845841083a89dfff0edf74
danielm <danielmontiel1600@gmail.com>
2024-04-05 19:40:39 -0500

    start with sector MVC

--- SimulacroCSharp/SimulacroCSharp.Mvc/Controllers/SectorsController.cs
+++ SimulacroCSharp/SimulacroCSharp.Mvc/Controllers/SectorsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SimulacroCSharp.Mvc.Data;
+using SimulacroCSharp.Mvc.Models;
 
 namespace SimulacroCSharp.Mvc.Controllers 
 {
@@ -18,5 +19,58 @@ namespace SimulacroCSharp.Mvc.Controllers
       return View(await _context.Sectors.ToListAsync());
     }
 
+    public async Task<IActionResult> Detail (int Id)
+    {
+      return View(await _context.Sectors.FirstOrDefaultAsync(register => register.Id == Id));
+    }
+
+    // Modify data
+    public IActionResult Create ()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create (Sector s)
+    {
+      _context.Companies.Add(s);
+      _context.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Update (int Id)
+    {
+      return View(await _context.Companies.FirstOrDefaultAsync(register => register.Id == Id));
+    }
+
+    [HttpPost]
+    public IActionResult Update (Sector s)
+    {
+      _context.Sectors.Update(s);
+      _context.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    // Delete
+    public async Task<IActionResult> Delete (int Id)
+    {
+      var user = await _context.Sectors.FindAsync(Id);
+      _context.Sectors.Remove(user);
+      _context.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    // Search
+    public IActionResult Search (string searchString)
+    {
+      var sector = _context.Sectors.AsQueryable();
+      // var users = from user in _context.Companies select user;
+      if (!string.IsNullOrEmpty(searchString))
+      {
+        sector = sector.Where(s => s.Name.Contains(searchString));
+      }
+
+      return View("Search", sector.ToList());
+    }
   }
 }
--- SimulacroCSharp/SimulacroCSharp.Mvc/Models/Sector.cs
+++ SimulacroCSharp/SimulacroCSharp.Mvc/Models/Sector.cs
@@ -6,6 +6,6 @@ namespace SimulacroCSharp.Mvc.Models
     public int Id {get; set;}
     public string Name {get; set;}
     public string Description {get; set;}
-    public string Autor {get; set;}
+    public string Author {get; set;}
   }
 }
