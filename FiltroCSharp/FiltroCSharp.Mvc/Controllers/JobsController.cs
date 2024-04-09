b34395c8f3651a158e8c690d8fc5f17b6b3ddb08
dannyKentala <handres41@outlook.com>
2024-04-09 15:15:18 -0500

    reference jobs in controller

--- FiltroCSharp/FiltroCSharp.Mvc/Controllers/JobsController.cs
+++ FiltroCSharp/FiltroCSharp.Mvc/Controllers/JobsController.cs
@@ -1,9 +1,15 @@
+using FiltroCSharp.Mvc.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FiltroCSharp.Mvc
 {
   public class JobsController : Controller 
   {
-    
+    public readonly BaseContext _context;
+
+    public JobsController (BaseContext context)
+    {
+      _context = context;
+    } 
   }
 }
